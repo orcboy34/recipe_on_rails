0329bfc5accab5ae4ccbea673eb8f48cc4d6db0f
Ryan Robar <ryanrobar@live.com>
2026-02-19 09:59:29 -0400

    Added Tags

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_02_18_231330) do
+ActiveRecord::Schema[8.1].define(version: 2026_02_19_031846) do
   create_schema "extensions"
 
   # These are extensions that must be enabled in order to support this database
@@ -28,6 +28,13 @@ ActiveRecord::Schema[8.1].define(version: 2026_02_18_231330) do
     t.string "user_agent"
     t.bigint "user_id", null: false
     t.index ["user_id"], name: "index_sessions_on_user_id"
+  end
+
+  create_table "public.tags", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.text "description"
+    t.string "name"
+    t.datetime "updated_at", null: false
   end
 
   create_table "public.users", force: :cascade do |t|
