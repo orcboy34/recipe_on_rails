96b76f015e251a75254a6fa234d8662465d7836f
Ryan Robar <ryanrobar@live.com>
2026-02-20 15:56:34 -0400

    Trying to remove Graphql

--- db/schema.rb
+++ db/schema.rb
@@ -17,7 +17,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_02_19_031846) do
   enable_extension "extensions.pg_stat_statements"
   enable_extension "extensions.pgcrypto"
   enable_extension "extensions.uuid-ossp"
-  enable_extension "graphql.pg_graphql"
+  #enable_extension "graphql.pg_graphql"
   enable_extension "pg_catalog.plpgsql"
   enable_extension "vault.supabase_vault"
 
