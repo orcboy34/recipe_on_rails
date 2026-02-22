086371e18d38d60d7011e5a560ae6e29f582175a
Ryan Robar <ryanrobar@live.com>
2026-02-22 13:10:55 -0400

    Updated home page unit tests

--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -1,7 +1,12 @@
 require "test_helper"
 
 class PagesControllerTest < ActionDispatch::IntegrationTest
+  test "should get login" do
+    get pages_home_url
+    assert_redirected_to new_session_path
+  end
   test "should get home" do
+    sign_in_as(users(:one))
     get pages_home_url
     assert_response :success
   end
