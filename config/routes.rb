9aa20123b67339f4befdde689f48f3a347ab5f6f
Ryan Robar <ryanrobar@live.com>
2026-02-23 18:15:05 -0400

    Generated the Recipe model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :recipes
   resources :ingredients
   get "pages/home"
   resources :units
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_02_23_184505) do
+ActiveRecord::Schema[8.1].define(version: 2026_02_23_214004) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -22,6 +22,14 @@ ActiveRecord::Schema[8.1].define(version: 2026_02_23_184505) do
   create_table "ingredients", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.text "description"
+    t.string "name"
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "recipes", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.text "description"
+    t.text "directions"
     t.string "name"
     t.datetime "updated_at", null: false
   end
