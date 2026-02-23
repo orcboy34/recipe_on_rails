5feb4d8eea88f1b7fb7940f73ba1c12649b311d9
Ryan Robar <ryanrobar@live.com>
2026-02-22 22:29:45 -0400

    Added Font Awesome icons and overhauled the tag view

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def fa_icon(icon_name, options = {})
+    # Default to solid style unless specified
+    style = options.delete(:style) || "fa-solid"
+    classes = [ style, "fa-#{icon_name}", options.delete(:class) ].compact.join(" ")
+
+    content_tag(:i, nil, options.merge(class: classes))
+  end
 end
