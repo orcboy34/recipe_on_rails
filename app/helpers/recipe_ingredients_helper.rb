ac504104828c75ff25dddb303dfa04fb0a0075a5
Ryan Robar <ryanrobar@live.com>
2026-02-25 11:58:58 -0400

    Changed unit display to use the abbreviation instead of the name

--- app/helpers/recipe_ingredients_helper.rb
+++ app/helpers/recipe_ingredients_helper.rb
@@ -18,9 +18,9 @@ module RecipeIngredientsHelper
     fraction_char = FRACTION_MAP[fractional_amount]
 
     if fraction_char
-      "#{whole_amount > 0 ? whole_amount : ''}#{fraction_char} #{recipe_ingredient.unit.name}#{recipe_ingredient.amount > 1 ? 's' : '' } #{recipe_ingredient.ingredient.name}".strip
+      "#{whole_amount > 0 ? whole_amount : ''}#{fraction_char} #{recipe_ingredient.unit.abbreviation}#{recipe_ingredient.amount > 1 ? 's' : '' } #{recipe_ingredient.ingredient.name}".strip
     else
-      "#{pluralize(whole_amount, recipe_ingredient.unit.name)} #{recipe_ingredient.ingredient.name}"
+      "#{pluralize(whole_amount, recipe_ingredient.unit.abbreviation)} #{recipe_ingredient.ingredient.name}"
     end
   end
 end
