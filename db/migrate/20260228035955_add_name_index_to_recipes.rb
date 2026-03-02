621222767114fa70b87abf11457583e3a8e27f64
Ryan Robar <ryanrobar@live.com>
2026-03-02 16:41:12 -0400

    Fixed migration name

--- db/migrate/20260228035955_add_name_index_to_recipes.rb
+++ db/migrate/20260228035955_add_name_index_to_recipes.rb
@@ -1,4 +1,4 @@
-class AddNameIndexToRecipe < ActiveRecord::Migration[8.1]
+class AddNameIndexToRecipes < ActiveRecord::Migration[8.1]
   def change
     add_index :recipes, "lower(name)", name: "index_recipes_on_lower_name", unique: true
   end
